4b389a4a8e2150a69c2b13ed397f78cca5153c7a
coder1973 <bulentozturk2019@outlook.com>
2022-01-10 18:44:37 +0100

    add CreateJavaFile.java

---
+++ src/day_1/CreateJavaFile.java
@@ -0,0 +1,14 @@
+package day_1;
+
+public class CreateJavaFile {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int y;
+        if(x==2) {
+            y = 7;
+            System.out.println(y);
+        }
+
+    }
+}
